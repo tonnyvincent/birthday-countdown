3f3c10b40034ede22e44cd5f02c02f271713ba26
tonnyvincent <154866986+tonnyvincent@users.noreply.github.com>
2024-05-13 23:58:22 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Apr 14, 2024',
+  birthdate: 'May 14, 2024',
   name: 'Biraa'
 };
 
