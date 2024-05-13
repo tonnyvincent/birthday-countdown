19f36b9a417cc896970a5e5d0f6e16365c06c6ee
tonnyvincent <154866986+tonnyvincent@users.noreply.github.com>
2024-05-13 23:56:56 +0800

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
-  name: 'Darlene'
+  birthdate: 'Apr 14, 2024',
+  name: 'Biraa'
 };
 
 function hideEverything() {
